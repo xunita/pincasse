054a2c09bfbb3da604cb4353ac968d9fd06d6be9
sopegue <yayasopeguesoro@gmail.com>
2020-12-26 23:57:18 +0100

    wip

--- components/search/Searchbar.vue
+++ components/search/Searchbar.vue
@@ -10,14 +10,16 @@
     @mouseleave="manageleave"
   >
     <input
-      class="w-full outline-none py-15 pl-3 font-medium font-semibold"
+      class="w-full h-full outline-none py-15 pl-3 font-medium font-semibold border-l rounded border-transparent"
       type="search"
       placeholder="Vue Composition API"
       @focus="managefocus"
       @blur="manageblur"
     />
     <client-only>
-      <button class="bg-white hover-008489 no-outline btn-search p-2 mr-1">
+      <button
+        class="bg-white hover-008489 no-outline btn-search p-2 h-full border-r border-transparent"
+      >
         <i class="fas fa-search color-363636 makeme-008489"></i>
       </button>
     </client-only>
