0109ee91b83b965d521d7f5e5acedbf2c03ca32d
sopegue <yayasopeguesoro@gmail.com>
2021-01-04 04:52:04 +0100

    wip

--- pages/home/courses/search.vue
+++ pages/home/courses/search.vue
@@ -3,10 +3,11 @@
     <div class="mb-10">
       <p>
         <span class="size-15 color-363636"
-          >Results for "<strong>Vue j</strong>", 327 found ! Try "<a
+          >Result(s) for "<strong>Vue j</strong>",
+          <span class="font-semibold">327</span> found. Try "<a
             class="color-008489 clickable font-semibold"
             >Vue js</a
-          >"</span
+          >" !</span
         >
       </p>
     </div>
