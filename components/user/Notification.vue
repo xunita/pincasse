7821fe7b9591157cc380569fad1227b365e5f46c
sopegue <yayasopeguesoro@gmail.com>
2021-01-13 03:57:57 +0100

    wip

--- components/user/Notification.vue
+++ components/user/Notification.vue
@@ -6,8 +6,7 @@
     <div
       class="flex w-full whatitis align-center justify-between pb-2 border-b mb-8 space-x-8"
     >
-      <div class="z-30"><Sort /></div>
-      <div class="w-8/12 xl:w-5/12 lg:w-1/2 md:w-8/12 sm:w-9/12 self-end">
+      <div class="w-full xl:w-5/12 lg:w-1/2 md:w-8/12 sm:w-9/12 self-end">
         <Searchbarsimple />
       </div>
     </div>
