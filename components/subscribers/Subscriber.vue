25eda9058507e2e56b634ff54164eee100e34c3a
sopegue <yayasopeguesoro@gmail.com>
2021-01-15 03:15:52 +0100

    wip

--- components/subscribers/Subscriber.vue
+++ components/subscribers/Subscriber.vue
@@ -66,7 +66,7 @@
             class="bg-color-008489p mt-3 max-sub size-13 font-semibold outline-none no-outline rounded text-white py-1 bghover-008489 hover-white active-white border-0"
           >
             <span class="px-3"
-              >Subscribe
+              >Subscribed
               <svg
                 class="w-5 h-5 inline-block"
                 fill="currentColor"
@@ -75,10 +75,11 @@
               >
                 <path
                   fill-rule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
                   clip-rule="evenodd"
-                ></path></svg
-            ></span>
+                ></path>
+              </svg>
+            </span>
           </button>
         </div>
       </div>
