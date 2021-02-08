e76145deb3a2597fc563ac961a7fe7e7ab93be74
sopegue <yayasopeguesoro@gmail.com>
2021-02-08 22:42:10 +0100

    wip

--- pages/index/courses/_cr/_title.vue
+++ pages/index/courses/_cr/_title.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="popo lg:w-4/5 md:w-3/4 sm:w-3/5 w-full lg:pr-6 md:pr-6">
-    <div class="w-full h-auto border-b pb-6">
-      <p class="relative w-11/12 m-0-auto vvss">
+    <div class="w-full h-fit">
+      <div class="relative w-11/12 m-0-auto vvss">
         <video
           id="my-video"
           class="video-js vjs-theme-sea"
@@ -12,56 +12,56 @@
         >
           <source src="/vid.mp4" type="video/mp4" />
         </video>
-      </p>
-      <div class="w-11/12 m-0-auto relative pipin">
-        <div class="flex align-center">
-          <button
-            class="button color-363636 rounded is-lights no-outline border-0 border-tr font-medium size-14 flex align-center py-2"
-          >
-            <svg
-              class="w-5 h-5 color-363636"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+        <div class="w-full m-0-auto relative pipin border-b pb-2">
+          <div class="flex align-center">
+            <button
+              class="button color-363636 rounded is-lights no-outline border-0 border-tr font-medium size-14 flex align-center py-2"
             >
-              <path
-                d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"
-              ></path>
-            </svg>
-            <span class="block ml-2 size-13"> 87k likes</span>
-          </button>
-          <button
-            class="button color-363636 rounded is-lights no-outline border-0 border-tr font-medium size-14 flex align-center py-2"
-          >
-            <svg
-              class="w-5 h-5 color-363636"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+              <svg
+                class="w-5 h-5 color-363636"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"
+                ></path>
+              </svg>
+              <span class="block ml-2 size-13"> 87k likes</span>
+            </button>
+            <button
+              class="button color-363636 rounded is-lights no-outline border-0 border-tr font-medium size-14 flex align-center py-2"
             >
-              <path
-                fill-rule="evenodd"
-                d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span class="block ml-2 size-13"> 5.4k comments</span>
-          </button>
-          <button
-            class="button color-363636 rounded is-lights no-outline border-0 border-tr font-medium size-14 flex align-center py-2"
-          >
-            <svg
-              class="w-5 h-5 color-363636"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+              <svg
+                class="w-5 h-5 color-363636"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span class="block ml-2 size-13"> 5.4k comments</span>
+            </button>
+            <button
+              class="button color-363636 rounded is-lights no-outline border-0 border-tr font-medium size-14 flex align-center py-2"
             >
-              <path
-                d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"
-              ></path>
-            </svg>
-            <span class="block ml-2 size-13">800 Shares</span>
-          </button>
+              <svg
+                class="w-5 h-5 color-363636"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"
+                ></path>
+              </svg>
+              <span class="block ml-2 size-13">800 Shares</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -115,7 +115,7 @@ export default {
   margin-left: 2.25rem !important;
 }
 .pipin {
-  top: 0.9rem !important;
+  top: 1rem !important;
 }
 .sidemiddle {
   width: 100% !important;
